c518bcc6cf969a0b0c1f9f6406f6851206519217
Arto Bendiken <arto@bendiken.net>
2013-04-01 23:46:46 +0200

    Defined the libposix++ subproject.

---
+++ feature.h
@@ -0,0 +1,11 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef POSIXXX_FEATURE_H
+#define POSIXXX_FEATURE_H
+
+namespace posix {
+  namespace feature {
+  }
+}
+
+#endif /* POSIXXX_FEATURE_H */
---
+++ module.h
@@ -0,0 +1,11 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef POSIXXX_MODULE_H
+#define POSIXXX_MODULE_H
+
+namespace posix {
+  namespace module {
+  }
+}
+
+#endif /* POSIXXX_MODULE_H */
