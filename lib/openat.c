94d7f1cc1ef5464d7783a9c15796fb2d1763b19a
Arto Bendiken <arto@bendiken.net>
2013-05-07 01:49:06 +0200

    Defined the openat() compatibility function.

---
+++ lib/openat.c
@@ -0,0 +1,15 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#include "openat.h"
+
+#include <errno.h>    /* for E*, errno */
+#include <sys/stat.h> /* for mode_t */
+
+int
+openat(const int dirfd,
+       const char* const pathname,
+       const int flags, ...) {
+  (void)dirfd, (void)pathname, (void)flags;
+
+  return errno = ENOSYS, -1; // TODO
+}
---
+++ lib/openat.h
@@ -0,0 +1,38 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef POSIXXX_OPENAT_H
+#define POSIXXX_OPENAT_H
+
+/**
+ * A public domain implementation of the POSIX.1-2008 openat() function.
+ *
+ * @see http://pubs.opengroup.org/onlinepubs/9699919799/functions/open.html
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * A special `dirfd` value used to indicate that `openat()` should operate
+ * relative to the current working directory.
+ */
+#define AT_FDCWD -100
+
+/**
+ * Opens a file relative to a directory file descriptor.
+ *
+ * @param dirfd
+ * @param pathname
+ * @param flags
+ * @param mode
+ * @return a new file descriptor, or -1 on error
+ * @see http://pubs.opengroup.org/onlinepubs/9699919799/functions/open.html
+ */
+int openat(int dirfd, const char* pathname, int flags, ...);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* POSIXXX_OPENAT_H */
