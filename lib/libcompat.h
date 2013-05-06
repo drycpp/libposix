88d56daf118b902f9f0234fc0d6757a1bd55b5b6
Arto Bendiken <arto@bendiken.net>
2013-05-07 01:31:36 +0200

    Defined a libcompat library for implementing missing system functions.
    See: http://www.gnu.org/software/automake/manual/html_node/LIBOBJS.html

---
+++ lib/libcompat.h
@@ -0,0 +1,10 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef LIBCOMPAT_H
+#define LIBCOMPAT_H
+
+#ifndef HAVE_OPENAT
+#include "openat.h"
+#endif
+
+#endif /* LIBCOMPAT_H */
