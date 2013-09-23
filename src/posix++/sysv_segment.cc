7a0b5244abf5206bf55b07371e3ddefa33cfd4fa
Arto Bendiken <arto@bendiken.net>
2013-09-23 02:26:53 +0200

    Implemented the posix::sysv_segment class.

--- src/posix++/sysv_segment.cc
+++ src/posix++/sysv_segment.cc
@@ -4,12 +4,183 @@
 #include <config.h>
 #endif
 
-#include "sysv_segment.h"
-
 #include "error.h"
+#include "sysv_segment.h"
 
 #include <cassert>      /* for assert() */
 #include <cerrno>       /* for errno */
-#include <sys/shm.h>    /* for struct shmid_ds */
+#include <sys/types.h>  /* for key_t */
+#include <sys/ipc.h>    /* for shm*() on BSD */
+#include <sys/shm.h>    /* for struct shmid_ds, shm*() */
 
 using namespace posix;
+
+/**
+ * @see http://pubs.opengroup.org/onlinepubs/9699919799/functions/shmget.html
+ * @see http://man7.org/linux/man-pages/man2/shmget.2.html
+ * @see http://www.freebsd.org/cgi/man.cgi?query=shmget&sektion=2
+ * @see http://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man2/shmget.2.html
+ */
+sysv_segment
+sysv_segment::create(const key_t key,
+                     const std::size_t size,
+                     const int flags) {
+  int shmid;
+  if ((shmid = shmget(key, size, IPC_CREAT | flags)) == -1) {
+    switch (errno) {
+      case EINVAL: /* Invalid argument */
+        throw posix::invalid_argument();
+      case ENOMEM: /* Cannot allocate memory in kernel */
+      case ENOSPC: /* No space left on device */
+        throw posix::fatal_error(errno);
+      default:
+        throw posix::runtime_error(errno);
+    }
+  }
+  return sysv_segment(shmid);
+}
+
+/**
+ * @see http://pubs.opengroup.org/onlinepubs/9699919799/functions/shmget.html
+ * @see http://man7.org/linux/man-pages/man2/shmget.2.html
+ * @see http://www.freebsd.org/cgi/man.cgi?query=shmget&sektion=2
+ * @see http://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man2/shmget.2.html
+ */
+sysv_segment
+sysv_segment::open(const key_t key) {
+  int shmid;
+  if ((shmid = shmget(key, 0, 0)) == -1) {
+    switch (errno) {
+      case EINVAL: /* Invalid argument */
+        throw posix::invalid_argument();
+      case ENOMEM: /* Cannot allocate memory in kernel */
+        throw posix::fatal_error(errno);
+      default:
+        assert(errno != ENOSPC);
+        throw posix::runtime_error(errno);
+    }
+  }
+  return sysv_segment(shmid);
+}
+
+sysv_segment::~sysv_segment() noexcept {
+  if (_addr) {
+    if (shmdt(_addr) == -1) {
+      /* Ignore any errors from shmdt(). */
+    }
+    _addr = nullptr;
+  }
+  if (_id) {
+#if 0 // TODO: think this through.
+    if (shmctl(_id, IPC_RMID, nullptr) == -1) {
+      /* Ignore any errors from shmctl(). */
+    }
+#endif
+    _id = -1;
+  }
+}
+
+/**
+ * @see http://pubs.opengroup.org/onlinepubs/9699919799/functions/shmat.html
+ * @see http://man7.org/linux/man-pages/man2/shmat.2.html
+ * @see http://www.freebsd.org/cgi/man.cgi?query=shmat&sektion=2
+ * @see http://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man2/shmat.2.html
+ */
+void*
+sysv_segment::attach(const int flags) {
+  if (!_addr) {
+    if ((_addr = shmat(_id, nullptr, flags)) == reinterpret_cast<void*>(-1)) {
+      switch (errno) {
+        case EINVAL: /* Invalid argument */
+          throw posix::invalid_argument();
+        case EMFILE: /* Too many open segments */
+        case ENOMEM: /* Cannot allocate memory in kernel */
+          throw posix::fatal_error(errno);
+        default:
+          throw posix::runtime_error(errno);
+      }
+    }
+    assert(_addr != nullptr);
+  }
+  return _addr;
+}
+
+/**
+ * @see http://pubs.opengroup.org/onlinepubs/9699919799/functions/shmdt.html
+ * @see http://man7.org/linux/man-pages/man2/shmdt.2.html
+ * @see http://www.freebsd.org/cgi/man.cgi?query=shmdt&sektion=2
+ * @see http://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man2/shmdt.2.html
+ */
+void
+sysv_segment::detach() {
+  if (_addr) {
+    if (shmdt(_addr) == -1) {
+      switch (errno) {
+        case EINVAL: /* Invalid argument */
+          throw posix::invalid_argument();
+        default:
+          throw posix::runtime_error(errno);
+      }
+    }
+    _addr = nullptr;
+  }
+}
+
+/**
+ * @see http://pubs.opengroup.org/onlinepubs/9699919799/functions/shmctl.html
+ * @see http://man7.org/linux/man-pages/man2/shmctl.2.html
+ * @see http://www.freebsd.org/cgi/man.cgi?query=shmctl&sektion=2
+ * @see http://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man2/shmctl.2.html
+ */
+void
+sysv_segment::remove() {
+  if (_id) {
+    if (shmctl(_id, IPC_RMID, nullptr) == -1) {
+      switch (errno) {
+        case EINVAL: /* Invalid argument */
+          throw posix::invalid_argument();
+        default:
+          assert(errno != EFAULT);
+          throw posix::runtime_error(errno);
+      }
+    }
+    _id = -1;
+  }
+}
+
+void
+sysv_segment::lock() {
+#ifdef SHM_LOCK
+  if (shmctl(_id, SHM_LOCK, nullptr) == -1) {
+    switch (errno) {
+      case EINVAL: /* Invalid argument */
+        throw posix::invalid_argument();
+      case ENOMEM: /* Cannot allocate memory in kernel */
+        throw posix::fatal_error(errno);
+      default:
+        assert(errno != EFAULT);
+        throw posix::runtime_error(errno);
+    }
+  }
+#else
+  throw posix::runtime_error(ENOSYS); /* Not implemented */
+#endif
+}
+
+void
+sysv_segment::unlock() {
+#ifdef SHM_UNLOCK
+  if (shmctl(_id, SHM_UNLOCK, nullptr) == -1) {
+    switch (errno) {
+      case EINVAL: /* Invalid argument */
+        throw posix::invalid_argument();
+      default:
+        assert(errno != ENOMEM);
+        assert(errno != EFAULT);
+        throw posix::runtime_error(errno);
+    }
+  }
+#else
+  throw posix::runtime_error(ENOSYS); /* Not implemented */
+#endif
+}
--- src/posix++/sysv_segment.h
+++ src/posix++/sysv_segment.h
@@ -3,29 +3,179 @@
 #ifndef POSIXXX_SYSV_SEGMENT_H
 #define POSIXXX_SYSV_SEGMENT_H
 
+#include <cstddef>     /* for std::size_t */
+#include <cstdint>     /* for std::uint8_t */
+#include <sys/types.h> /* for key_t */
+
 namespace posix {
   class sysv_segment;
 }
 
 /**
  * Represents a System V shared memory segment.
+ *
+ * @note Instances of this class are movable, but not copyable.
+ * @see http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap03.html#tag_03_342
  */
 class posix::sysv_segment {
 public:
   /**
-   * Constructor.
+   * Creates a new segment.
+   *
+   * @throws posix::error on failure
    */
-  sysv_segment(const int id) : _id(id) {}
+  static sysv_segment create(key_t key, std::size_t size, int flags = 0);
 
   /**
-   * Returns the integer identifier for this sysv_segment.
+   * Opens an existing segment.
+   *
+   * @throws posix::error on failure
    */
-  inline int id() const {
+  static sysv_segment open(key_t key);
+
+  /**
+   * Default constructor.
+   */
+  sysv_segment() noexcept = delete;
+
+  /**
+   * Constructor.
+   */
+  sysv_segment(const int shmid,
+               void* const shmaddr = nullptr)
+    : _id(shmid),
+      _addr(shmaddr) {}
+
+  /**
+   * Copy constructor.
+   */
+  sysv_segment(const sysv_segment& other) noexcept = delete;
+
+  /**
+   * Move constructor.
+   */
+  sysv_segment(sysv_segment&& other) noexcept = default;
+
+  /**
+   * Destructor.
+   */
+  ~sysv_segment() noexcept;
+
+  /**
+   * Copy assignment operator.
+   */
+  sysv_segment& operator=(const sysv_segment& other) noexcept = delete;
+
+  /**
+   * Move assignment operator.
+   */
+  sysv_segment& operator=(sysv_segment&& other) noexcept = default;
+
+  /**
+   * Determines whether the segment is attached.
+   */
+  inline bool is_attached() const noexcept {
+    return _addr;
+  }
+
+  /**
+   * Alias for `is_attached()`.
+   *
+   * @copydetails is_attached()
+   */
+  inline bool is_mapped() const noexcept {
+    return is_attached();
+  }
+
+  /**
+   * Returns the integer identifier for this segment.
+   */
+  inline int id() const noexcept {
     return _id;
   }
 
+  /**
+   * Returns a pointer to the attached memory segment.
+   *
+   * @pre `is_attached()` must be `true`.
+   */
+  template <typename T>
+  inline const T* data(const std::size_t offset = 0) const noexcept {
+    return reinterpret_cast<const T*>(data(offset));
+  }
+
+  /**
+   * Returns a pointer to the attached memory segment.
+   *
+   * @pre `is_attached()` must be `true`.
+   */
+  template <typename T>
+  inline T* data(const std::size_t offset = 0) noexcept {
+    return reinterpret_cast<T*>(data(offset));
+  }
+
+  /**
+   * Returns a pointer to the attached memory segment.
+   *
+   * @pre `is_attached()` must be `true`.
+   */
+  inline const std::uint8_t* data(const std::size_t offset = 0) const noexcept {
+    return reinterpret_cast<const std::uint8_t*>(_addr) + offset;
+  }
+
+  /**
+   * Returns a pointer to the attached memory segment.
+   *
+   * @pre `is_attached()` must be `true`.
+   */
+  inline std::uint8_t* data(const std::size_t offset = 0) noexcept {
+    return reinterpret_cast<std::uint8_t*>(_addr) + offset;
+  }
+
+  /**
+   * Attaches this segment to the process address space.
+   *
+   * @throws posix::error on failure
+   * @note This operation is idempotent.
+   */
+  void* attach(int flags = 0);
+
+  /**
+   * Detaches this segment from the process address space.
+   *
+   * @throws posix::error on failure
+   * @note This operation is idempotent.
+   */
+  void detach();
+
+  /**
+   * Marks this segment to be destroyed. The segment will only actually be
+   * removed after the last process detaches from it.
+   *
+   * @throws posix::error on failure
+   * @note This operation is idempotent.
+   */
+  void remove();
+
+  /**
+   * Locks this segment into resident memory.
+   *
+   * @throws posix::error on failure
+   * @note This operation is Linux-specific.
+   */
+  void lock();
+
+  /**
+   * Unlocks this segment, allowing it to be swapped out.
+   *
+   * @throws posix::error on failure
+   * @note This operation is Linux-specific.
+   */
+  void unlock();
+
 protected:
   int _id = -1;
+  void* _addr = nullptr;
 };
 
 #endif /* POSIXXX_SYSV_SEGMENT_H */
