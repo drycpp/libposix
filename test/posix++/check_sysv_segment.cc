c684f6ed83d54e16e1b8e30ecf0fb67a72c88e1d
Arto Bendiken <arto@bendiken.net>
2013-09-23 03:25:00 +0200

    Added a trivial unit test for posix::sysv_segment.

--- test/posix++/check_sysv_segment.cc
+++ test/posix++/check_sysv_segment.cc
@@ -5,6 +5,16 @@
 
 #include <posix++/sysv_segment.h> /* for posix::sysv_segment */
 
+#include <unistd.h> /* for getpagesize(), getpid() */
+
+using namespace posix;
+
 BOOST_AUTO_TEST_CASE(test_sysv_segment) {
-  BOOST_CHECK(true); // TODO
+  sysv_segment shm = sysv_segment::create(getpid(), getpagesize(), 0600);
+  BOOST_CHECK(!shm.is_attached());
+  shm.attach();
+  BOOST_CHECK(shm.is_attached());
+  shm.detach();
+  BOOST_CHECK(!shm.is_attached());
+  shm.remove();
 }
